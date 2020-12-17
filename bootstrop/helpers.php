c70ad230ed54dfca49d66233df222736367daaa5
rainsen <yusen@rainsen.comcd>
2020-12-17 10:43:42 +0800

    helpers

--- bootstrop/helpers.php
+++ bootstrop/helpers.php
@@ -1,5 +1,21 @@
 <?php
 
+if (! function_exists('_recursive')) {
+	
+	function _recursive(array $data, $parentField, $parentId = 0, $level = 0) {
+		static $orderedData = [];
+		foreach ($data as $key => $value) {
+			if ($value[$parentField] === $parentId) {
+				$value['level'] = $level;
+				$orderedData[] = $value;
+				unset($data[$key]);
+				_recursive($data, $parentField, $value['id'], $level+1);
+			}
+		}
+		return $orderedData;
+	}
+}
+
 if (! function_exists('widget_base_path')) {
 	function widget_base_path(string $path = '') {
 		return dirname(__DIR__) . ($path ? DIRECTORY_SEPARATOR . $path : $path);
@@ -16,21 +32,5 @@ if (! function_exists('widget_resource_path')) {
 if (! function_exists('widget_view_path')) {
 	function widget_view_path(string $path = '') {
 		return widget_resource_path('view') . ($path ? DIRECTORY_SEPARATOR . $path : $path);
-	}
-}
-
-if (! function_exists('_recursive')) {
-	
-	function _recursive(array $data, $parentField, $parentId = 0, $level = 0) {
-		static $orderedData = [];
-		foreach ($data as $key => $value) {
-			if ($value[$parentField] === $parentId) {
-				$value['level'] = $level;
-				$orderedData[] = $value;
-				unset($data[$key]);
-				_recursive($data, $parentField, $value['id'], $level+1);
-			}
-		}
-		return $orderedData;
 	}
 }
